0961cdd6f17b531b681e69a9c61db1fd94aa3c0e
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-10 15:33:49 +0530

    Update p7.java

--- p7.java
+++ p7.java
@@ -36,3 +36,158 @@ public class Main
     
     	}
 }
+
+
+/*
+    1 
+   1 2 
+  1   3 
+ 1     4 
+1 2 3 4 5 
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    
+    for(int i=1;i<=n;i++)
+        {
+            for(int k =0 ;k<=(n-i-1);k++)
+            {
+                System.out.print(" ");
+            }
+            
+           
+            for(int j=1;j<=i;j++)
+                {
+                    if(i==j || j==1 || i==n )
+                    {
+                        System.out.print(j + " ");
+                    }
+                   else{
+                       System.out.print("  ");
+                   }
+                 
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+/*
+    1 
+   2 2 
+  3   3 
+ 4     4 
+5 5 5 5 5 
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    
+    for(int i=1;i<=n;i++)
+        {
+            for(int k =0 ;k<=(n-i-1);k++)
+            {
+                System.out.print(" ");
+            }
+            
+           
+            for(int j=1;j<=i;j++)
+                {
+                    if(i==j || j==1 || i==n )
+                    {
+                        System.out.print(i + " ");
+                    }
+                   else{
+                       System.out.print("  ");
+                   }
+                 
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+/*
+1 
+1 2 
+1   3 
+1     4 
+1 2 3 4 5 
+
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    
+    for(int i=1;i<=n;i++)
+        {
+            
+            
+           
+            for(int j=1;j<=i;j++)
+                {
+                    if(i==j || i==n || j==1 )
+                    {
+                        System.out.print(j + " ");
+                    }
+                   else{
+                       System.out.print("  ");
+                   }
+                 
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+
+/*
+1 
+2 2 
+3   3 
+4     4 
+5 5 5 5 5 
+
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    
+    for(int i=1;i<=n;i++)
+        {
+            
+            
+           
+            for(int j=1;j<=i;j++)
+                {
+                    if(i==j || i==n || j==1 )
+                    {
+                        System.out.print(i + " ");
+                    }
+                   else{
+                       System.out.print("  ");
+                   }
+                 
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
