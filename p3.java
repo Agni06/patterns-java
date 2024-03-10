3cd095afe2873300ac5e9f3389affbdb0a250fb4
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-10 12:46:46 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,174 @@
+
+/*
+*
+**
+***
+****
+*****
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+    for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+                {
+                    
+                 System.out.print("*");
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+
+/*
+1
+12
+123
+1234
+12345
+
+*/
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+    for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+                {
+                    
+                 System.out.print(j);
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+/*
+1
+22
+333
+4444
+55555
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+    for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+                {
+                    
+                 System.out.print(i);
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+/*
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+    for(int i=1;i<=n;i++)
+        {
+            for(int k =0;k<=n-i-1;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+                {
+                    
+                 System.out.print("*");
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+
+/*
+    1
+   12
+  123
+ 1234
+12345
+
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+    for(int i=1;i<=n;i++)
+        {
+            for(int k =0;k<=n-i-1;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+                {
+                    
+                 System.out.print(j);
+                }
+                System.out.println();
+        }
+    
+    	}
+}
+
+
+
+/*
+    1
+   22
+  333
+ 4444
+55555
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+    for(int i=1;i<=n;i++)
+        {
+            for(int k =0;k<=n-i-1;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+                {
+                    
+                 System.out.print(i);
+                }
+                System.out.println();
+        }
+    
+    	}
+}
